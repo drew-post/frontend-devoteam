5e833a1697ca4ff6a98fa094d788b545e66fb6b6
dap177 <dap177@txstate.edu>
2022-02-10 12:18:46 -0600

    Fix contact info list spacing

--- src/app/meet-the-team/team-members.spec.ts
+++ src/app/meet-the-team/team-members.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TeamMembersComponent } from './team-members.component';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { User } from '../models/users.model';
+import { HttpClient } from '@angular/common/http';
 
 describe('TeamMembersComponent', () => {
   let component: TeamMembersComponent;
@@ -21,5 +23,9 @@ describe('TeamMembersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch api results', () => {
+    expect(component.ngOnInit()).toHaveBeenCalled();
   });
 });
