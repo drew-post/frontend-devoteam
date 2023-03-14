d5d75564cee23d592b019d024c5a4a7880817d14
drew-post <drew_post@outlook.com>
2023-03-14 15:22:12 -0500

    Initial unit tets

--- src/app/components/search-and-filter-bar/search-and-filter-bar.component.spec.ts
+++ src/app/components/search-and-filter-bar/search-and-filter-bar.component.spec.ts
@@ -22,4 +22,37 @@ describe('SearchAndFilterBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+    
+    //     it('should change view when view button is clicked', () => {
+    //       // Spy on method called when button is clicked
+    //       component.ngOnInit();
+    //       spyOn(component, 'changeDisplay');
+    
+    //       // Identify button and click
+    //       let button = fixture.debugElement.nativeElement.querySelector('button.members__list-view-icon');
+    //       button.click();
+    
+    //       // Detect changes and expect changeDisplay() method to have been called
+    //       fixture.detectChanges();
+    
+    //       fixture.whenStable().then(() => {
+    //         expect(component.changeDisplay).toHaveBeenCalled();
+    //       });
+    //     });
+    
+    //     it('should sort when sort button is clicked', () => {
+    //       // Spy on method called when button is clicked
+    //       spyOn(component, 'send');
+    
+    //       // Identify button and click
+    //       let button = fixture.debugElement.nativeElement.querySelector('button.members__sort-view-icon');
+    //       button.click();
+    
+    //       // Detect changes and expect send() method to have been called
+    //       fixture.detectChanges();
+    
+    //       fixture.whenStable().then(() => {
+    //         expect(component.send).toHaveBeenCalled();
+    //       });
+    //     });
 });
--- src/app/components/team-grid-view/team-grid-view.component.spec.ts
+++ src/app/components/team-grid-view/team-grid-view.component.spec.ts
@@ -22,4 +22,26 @@ describe('TeamGridViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //     it('should call fill grid member cards with information', () => {
+    //       // Call API
+    //       component.ngOnInit();
+    
+    //       // Name field
+    //       let name = fixture.debugElement.nativeElement.querySelector('p.members__name-grid');
+    //       expect(name).toBeTruthy;
+    
+    //       // Image field
+    //       let image = fixture.debugElement.nativeElement.querySelector('p.members__image-grid');
+    //       expect(image).toBeTruthy;
+    
+    //       // City field
+    //       let city = fixture.debugElement.nativeElement.querySelector('p.members__city-grid');
+    //       expect(city).toBeTruthy;
+    
+    //       // Contact field
+    //       let contactInfo = fixture.debugElement.nativeElement.querySelector('p.members__contact-info-grid');
+    //       expect(contactInfo).toBeTruthy;
+    
+    //     });
 });
